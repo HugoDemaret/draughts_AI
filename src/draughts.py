be0bde8a89d85e3893e2b2ae98b4edd88641e9ce
Hugo Demaret <hugovladimir2000@gmail.com>
2023-04-29 17:35:33 +0200

    working on the agents

--- src/draughts.py
+++ src/draughts.py
@@ -1,3 +1,5 @@
+import random
+
 # Checker Game in Python
 
 
@@ -194,6 +196,160 @@ class Board:
                         if row > 1 and col < 6 and self.board[row-1][col+1] in [self.BLACK, self.KING_BLACK] and self.board[row-2][col+2] == self.EMPTY:
                             moves.append((row, col, row-2, col+2))
         return moves
+
+
+
+###############################################################################################################
+# AGENTS ######################################################################################################
+###############################################################################################################
+
+
+
+"""
+Random agent
+"""
+def random_agent(board, player):
+    """
+    Returns a random move from the list of legal moves.
+    :param board: The current board state.
+    :param player: The player to move.
+    :return: A random move from the list of legal moves.
+    """
+    return random.choice(board.get_valid_moves(player))
+
+
+
+"""
+Intelligent agents
+"""
+
+
+
+def intelligent_agent(board, game_settings, player):
+    """
+    Returns an intelligent agent's move.
+    :param board:
+    :param game_settings:
+    :param player:
+    :return:
+    """
+
+    #Evalutation functions
+
+    def evaluate_greedy(board):
+        """
+        Evaluates the board for the greedy algorithm.
+        :param board:
+        :return:
+        """
+        # Get the number of pieces for each player
+        black_pieces = 0
+        white_pieces = 0
+        for row in board:
+            for piece in row:
+                if piece == 'b' or piece == 'B':
+                    black_pieces += 1
+                elif piece == 'w' or piece == 'W':
+                    white_pieces += 1
+        # Return the difference between the number of pieces
+        return black_pieces - white_pieces  # Positive is good for black, negative is good for white
+
+
+    def greedy(board, player):
+        """
+        Greedy algorithm
+        """
+        # Get a list of all legal moves
+        legal_moves = board.get_valid_moves(player)
+        # Get the best move
+        best_move = None
+        best_move_score = float('-inf')
+        for move in legal_moves:
+            # Make the move
+            board.make_move(move, player)
+            # Get the score of the move
+            move_score = evaluate_greedy(board)
+            # Undo the move
+            board.undo_move()
+            # Check if the move is better than the current best move
+            if move_score > best_move_score:
+                # If it is, update the best move
+                best_move = move
+                best_move_score = move_score
+        # Return the best move
+        return best_move
+
+    def alpha_beta_pruning(board, depth, alpha, beta, is_maximizing_player):
+        if is_maximizing_player:
+            max_eval = float('-inf')
+            for move in board.legal_moves:
+                board.make_move(move, player)
+                eval = alpha_beta_pruning(board, depth - 1, alpha, beta, False)
+                board.pop()
+                max_eval = max(max_eval, eval)
+                alpha = max(alpha, eval)
+                if beta <= alpha:
+                    break
+            return max_eval
+        else:
+            min_eval = float('inf')
+            for move in board.legal_moves:
+                board.push(move)
+                eval = alpha_beta_pruning(board, depth - 1, alpha, beta, True)
+                board.pop()
+                min_eval = min(min_eval, eval)
+                beta = min(beta, eval)
+                if beta <= alpha:
+                    break
+            return min_eval
+
+
+    # Logic of the function
+    if game_settings[player] == "1":
+        return greedy(board, player)
+    elif game_settings[player] == "2":
+        return alpha_beta_pruning(board, 3, float('-inf'), float('inf'), True)
+    elif game_settings[player] == "3":
+        return alpha_beta_pruning(board, 5, float('-inf'), float('inf'), True)
+    elif game_settings[player] == "4":
+        return alpha_beta_pruning(board, 10, float('-inf'), float('inf'), True)
+
+
+"""
+Human agent (hopefully intelligent)
+"""
+def human_agent(board, player):
+    """
+    Gets a move from the user.
+    :param board: The current board state.
+    :param player: The player to move.
+    :return: A move from the user.
+    """
+    while True:
+        try:
+            # Prompt the user to enter the row and column of the piece they want to move
+            start_row, start_col = map(int, input(
+                f"Player {player}'s turn. Enter the row and column of the piece you want to move (e.g. 1 2): ").split())
+            # Prompt the user to enter the row and column of the destination cell
+            end_row, end_col = map(int, input("Enter the row and column of the destination cell: ").split())
+            # Check if the move is valid
+            if board.is_valid_move(start_row, start_col, end_row, end_col, player):
+                # If the move is valid, return it as a tuple
+                return (start_row, start_col, end_row, end_col)
+            else:
+                # If the move is not valid, print an error message and prompt the user to enter a valid move
+                print("Invalid move. Please enter a valid move.")
+        except ValueError:
+            # If the user enters invalid input, print an error message and prompt them to enter valid input
+            print("Invalid input. Please enter valid input.")
+
+
+
+
+###############################################################################################################
+# GAME ########################################################################################################
+###############################################################################################################
+
 
 
 
